12d8e884833e32b1f828fd93e9e3a749056f1d7f
DeuX <danni.strom@elev.ga.lbs.se>
2023-01-09 17:46:01 +0100

    moon

--- Assets/Scripts/DayCycleController.cs
+++ Assets/Scripts/DayCycleController.cs
@@ -6,17 +6,25 @@ using UnityEngine.PlayerLoop;
 
 public class DayCycleController : MonoBehaviour
 {
-    [Range(0, 24)] public float timeOfDay = 1f;
+    [Range(0, 24)] public float timeOfDay;
 
     public GameObject sun;
     public Light sunLight;
 
-    public float timeMultiplier;
-    
-    void Start()
+    public GameObject moon;
+    public Light moonLight;
+
+    public float timeMultiplier = 1f;
+
+    public bool isNight;
+
+    private void Start()
     {
-        sun = gameObject;
-        sunLight = gameObject.GetComponent<Light>();
+        sun = GameObject.Find("Sun");
+        sunLight = sun.GetComponent<Light>();
+
+        moon = GameObject.Find("Moon");
+        moonLight = moon.GetComponent<Light>();
     }
 
     private void FixedUpdate()
@@ -24,6 +32,7 @@ public class DayCycleController : MonoBehaviour
         timeOfDay += Time.fixedDeltaTime * timeMultiplier;
         if (timeOfDay > 24) timeOfDay = 0;
         UpdateTime();
+        CheckNightDayTransition();
     }
 
     private void OnValidate()
@@ -35,6 +44,34 @@ public class DayCycleController : MonoBehaviour
     {
         float alpha = timeOfDay / 24f;
         float sunRotation = Mathf.Lerp(-90, 270, alpha);
+        float moonRotation = sunRotation - 180f;
         sun.transform.rotation = Quaternion.Euler(sunRotation, -150f, 0);
+        moon.transform.rotation = Quaternion.Euler(moonRotation, -150f, 0);
+    }
+
+    private void CheckNightDayTransition()
+    {
+        if (isNight)
+        {
+            if (moon.transform.rotation.eulerAngles.x > 180) StartDay();
+        }
+        else
+        {
+            if(sun.transform.rotation.eulerAngles.x > 180) StartNight();
+        }
+    }
+
+    private void StartDay()
+    {
+        isNight = false;
+        moonLight.shadows = LightShadows.None;
+        sunLight.shadows = LightShadows.Soft;
+    }
+
+    private void StartNight()
+    {
+        isNight = true;
+        sunLight.shadows = LightShadows.None;
+        moonLight.shadows = LightShadows.Soft;
     }
 }
