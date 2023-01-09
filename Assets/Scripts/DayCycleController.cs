6ee5949b626783940b043ef18b48e52c1b1f8197
DeuX <danni.strom@elev.ga.lbs.se>
2023-01-09 16:16:30 +0100

    Movement System

--- Assets/Scripts/DayCycleController.cs
+++ Assets/Scripts/DayCycleController.cs
@@ -1,18 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class DayCycleController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Range(0, 24)] public float timeOfDay;
+
+    public GameObject sun;
+    public Light sunLight;
+
+    public float timeMultiplier;
+    
     void Start()
     {
-        
+        sun = gameObject;
+        sunLight = gameObject.GetComponent<Light>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        timeOfDay += Time.fixedDeltaTime * timeMultiplier;
+        if (timeOfDay > 24) timeOfDay = 0;
+        UpdateTime();
+    }
+
+    private void OnValidate()
+    {
+        UpdateTime();
+    }
+
+    private void UpdateTime()
+    {
+        float alpha = timeOfDay / 24f;
+        float sunRotation = Mathf.Lerp(-90, 270, alpha);
+        sun.transform.rotation = Quaternion.Euler(sunRotation, -150f, 0);
     }
 }
--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -1,14 +1,10 @@
 ﻿using UnityEngine;
-#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
 using UnityEngine.InputSystem;
-#endif
 
 namespace StarterAssets
 {
 	[RequireComponent(typeof(CharacterController))]
-#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
 	[RequireComponent(typeof(PlayerInput))]
-#endif
 	public class FirstPersonController : MonoBehaviour
 	{
 		[Header("Player")]
@@ -64,10 +60,9 @@ namespace StarterAssets
 		private float _jumpTimeoutDelta;
 		private float _fallTimeoutDelta;
 
-	
-#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+		
 		private PlayerInput _playerInput;
-#endif
+
 		private CharacterController _controller;
 		private StarterAssetsInputs _input;
 		private GameObject _mainCamera;
@@ -78,11 +73,7 @@ namespace StarterAssets
 		{
 			get
 			{
-				#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
 				return _playerInput.currentControlScheme == "KeyboardMouse";
-				#else
-				return false;
-				#endif
 			}
 		}
 
@@ -99,11 +90,7 @@ namespace StarterAssets
 		{
 			_controller = GetComponent<CharacterController>();
 			_input = GetComponent<StarterAssetsInputs>();
-#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
 			_playerInput = GetComponent<PlayerInput>();
-#else
-			Debug.LogError( "Starter Assets package is missing dependencies. Please use Tools/Starter Assets/Reinstall Dependencies to fix it");
-#endif
 
 			// reset our timeouts on start
 			_jumpTimeoutDelta = JumpTimeout;
